02873fb792ab62cd457d2f769896c4e805c76f9c
wengtianchen <wengtianchen@foxmail.com>
2019-04-23 16:44:16 +0800

    bridge

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,5 +2,19 @@ new Vue({
     el:"#app",
     data:{
         
+    },
+    methods: {
+        getUsers(){
+            window.WebViewJavascriptBridge.send(
+                {"type":"echo"},
+                function (responseData) {
+                    console.log(responseData)
+                }
+            );
+        }
+    },
+    mounted () {
+        this.getUsers()
     }
+
 })
